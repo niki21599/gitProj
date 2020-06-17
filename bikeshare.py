d9dac55d73864bb1b138b3261b34d0602c4bb339
Niklas Burg <Niklasburg1999.nbgmail.com>
2020-06-17 15:11:29 +0200

    Changing function names

--- bikeshare.py
+++ bikeshare.py
@@ -73,7 +73,7 @@ def load_data(city, month, day):
     return df
 
 
-def time_stats(df):
+def time_stat(df):
     """Displays statistics on the most frequent times of travel."""
 
     print('\nCalculating The Most Frequent Times of Travel...\n')
@@ -157,7 +157,7 @@ def main():
         city, month, day = get_filters()
         df = load_data(city, month, day)
 
-        time_stats(df)
+        time_stat(df)
         station_stats(df)
         trip_duration_stats(df)
         user_stats(df)
